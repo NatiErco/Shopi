be40e451191b98c54c0abfcef405b883ec31013b
NatiErco <165024507+NatiErco@users.noreply.github.com>
2025-03-18 11:07:08 +0100

    Actualización de archivos y nuevos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { ShoppingCardProvider } from "./Context";
+import Home from "./Pages/Home";
+import MyAccount from "./Pages/MyAccount";
+import MyOrder from "./Pages/MyOrder";
+import MyOrders from "./Pages/MyOrders";
+import SignIn from "./Pages/SignIn";
+import NotFound from "./Pages/NotFound";
+import Navbar from "./Components/Navbar";
+import "./App.css";
 
 function App() {
-
   return (
-    <div className="bg-red-100">
-      Hola mundo bebé
-    </div>
-  )
+    <ShoppingCardProvider >
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-account" element={<MyAccount />} />
+        <Route path="/my-order" element={<MyOrder />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="*" element={<NotFound />} />{" "}
+        {/* Ruta para manejar errores 404 */}
+      </Routes>
+    </ShoppingCardProvider>
+  );
 }
 
-export default App
+export default App;
